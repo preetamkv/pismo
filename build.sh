d9bcfa7357ca736eac58e6782a1470525fa09b01
Venkata Preetam Kandula <kvpreetam@gmail.com>
2025-09-15 14:56:13 +0000

    Add comments in build script

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
+# Generate the executible
 go build ./...
 go build ./cmd/service/
 
+# Generate Docker image
 docker build -t pismo:latest .
+clear
+
+# Run the container using the image created
 docker run -p 8080:8080 pismo:latest
